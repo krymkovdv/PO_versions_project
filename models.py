f87210885adefe77a81db360e89a32677eed8bd1
krymkovdv <s89232844224@gmail.com>
2025-10-18 19:16:57 +0300

    bd_1.4

--- models.py
+++ models.py
@@ -27,43 +27,43 @@ class TractorComponent(Base):
 
     tractors = relationship('Tractors', back_populates='comp_list')
     
-    comp_rel = relationship('Components', back_populates='traccomp_rel') 
+    comp_rel = relationship('Components', back_populates='trac_comp_rel') 
 
 
 
-class Archiv_FW(Base):
-    __tablename__ = 'Arkhiv_fw'
+# class Archive_FW(Base):
+#     __tablename__ = 'Archive_fw'
 
-    id = Column(Integer, primary_key=True, index=True)
-    inner_version = Column(Text)
-    producer_version = Column(Text)
-    components = Column(Integer,ForeignKey('Components.id_comp'), unique=True)
+#     id = Column(Integer, primary_key=True, index=True)
+#     inner_version = Column(Text)
+#     producer_version = Column(Text)
+#     components = Column(Integer,ForeignKey('Components.id_comp'), unique=True)
 
-    comp_list = relationship('Components', back_populates='archive_list')
+#     comp_list = relationship('Components', back_populates='archive_list')
 
 
 #класс компонентов
-class Components(Base):
-    __tablename__ = 'Components'
+# class Components(Base):
+#     __tablename__ = 'Components'
 
-    id_comp = Column(Integer, primary_key=True, index=True)
-    fact_num_comp = Column(Text, nullable=False)
-    type_comp = Column(String(255), nullable=False)
-    model_comp = Column(Text, nullable=False)
-    year_comp = Column(DateTime)  
-    current_version = Column(Double(precision=53), ForeignKey("Firmwares.id_Firmwares"))  
-    recommended_version = Column(Double(precision=53), ForeignKey("Firmwares.id_Firmwares"))
-    maj_Min = Column(String(255))
-    time_cur = Column(DateTime)
-    time_rec = Column(DateTime)
-    time_m = Column(DateTime)
+#     id_comp = Column(Integer, primary_key=True, index=True)
+#     fact_num_comp = Column(Text, nullable=False)
+#     type_comp = Column(String(255), nullable=False)
+#     model_comp = Column(Text, nullable=False)
+#     year_comp = Column(DateTime)  
+#     current_version = Column(Double(precision=53), ForeignKey("Firmwares.id_Firmwares"))  
+#     recommended_version = Column(Double(precision=53), ForeignKey("Firmwares.id_Firmwares"))
+#     maj_Min = Column(String(255))
+#     time_cur = Column(DateTime)
+#     time_rec = Column(DateTime)
+#     time_m = Column(DateTime)
     
-    archive_list = relationship('Archive_FW', back_populates='comp_list', uselist=False)
+#     archive_list = relationship('Archive_FW', back_populates='comp_list', uselist=False)
 
-    firmware_recommended = relationship("Firmwares", back_populates="component_recommended")
-    firmware_current = relationship("Firmwares", back_populates="component_current")
-    trac_comp_rel = relationship('TractorComponent', back_populates='comp_rel', 
-                                 uselist=False)
+#     firmware_recommended = relationship("Firmwares", back_populates="component_recommended")
+#     firmware_current = relationship("Firmwares", back_populates="component_current")
+#     trac_comp_rel = relationship('TractorComponent', back_populates='comp_rel', 
+#                                  uselist=False)
 
 
 #класс прошивок
@@ -82,7 +82,9 @@ class Firmwares(Base):
    time_Min = Column(DateTime)
 
    # Обратная связь с компонентами
-   component_recommended = relationship("Component", back_populates="firmware_recommended", 
+   component_recommended = relationship("Components", back_populates="firmware_recommended",
                                     uselist=False)
-   component_current = relationship("Component", back_populates="firmware_current",
+   component_current = relationship("Components", back_populates="firmware_current",
                                     uselist=False)
+   
+   
