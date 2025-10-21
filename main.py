5be84046124a4f2fafc18139879bd09e6ae8fd0e
krymkovdv <s89232844224@gmail.com>
2025-10-21 21:23:25 +0300

    CRUD's start

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,select
 import psycopg2
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 import config
 from models import Base, Tractors,TractorComponent
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from schemas import TractorsSchema
 
 url_db = config.settings.get_url()
 
@@ -13,14 +14,34 @@ engine = create_engine(
     url_db,
 )
 
-Session = sessionmaker(bind=engine)
 
-with Session() as session:
-    with session.begin():
-        trac1 = Tractors(model ='743', region ='c1', owner_name = 'Vanya')
-        session.add(trac1)
-        
+
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+# 2. Функция зависимости — ОБЯЗАТЕЛЬНО до эндпоинтов!
+def get_session():
+    with SessionLocal() as session:
+        yield session
+
+
+# with Session() as session:
+#     with session.begin():
+#         trac1 = Tractors(model ='743', region ='c1', owner_name = 'Vanya')
+#         session.add(trac1)
+
+
 
 # Base.metadata.create_all(engine)
 #создание экземпляра приложения
 app = FastAPI(title="Сервис контроля версий")
+
+
+@app.get("/tractors/", response_model=list[TractorsSchema])
+def get_tractors(session: Session = Depends(get_session)):
+    tractors = session.execute(select(Tractors)).scalars().all()
+    return tractors  # FastAPI автоматически сериализует через Pydantic
+
+# uvicorn main:app
+# .\venv\Scripts\Activate.ps1
