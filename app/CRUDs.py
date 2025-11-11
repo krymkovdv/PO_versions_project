054c2aef2901ad8a327fad9a0ff292c89a5796e2
YobieYo <YobieYo2@gmail.com>
2025-11-11 20:29:15 +0300

    well

--- app/CRUDs.py
+++ app/CRUDs.py
@@ -1,9 +1,71 @@
 from sqlalchemy.orm import Session
-from . import models, schemas  
+from . import models, schemas
 from sqlalchemy import or_, cast, String, select
-from fastapi import HTTPException
+from fastapi import HTTPException, status, Depends
 from datetime import datetime
 from typing import List
+from .authorization import *
+from .models import *
+from datetime import timedelta
+from jose import JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+import re
+
+# Авторизация
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_user(db: AsyncSession, username: str):
+    result = await db.execute(select(UserDB).where(UserDB.username == username))
+    return result.scalars().first()
+
+
+async def authenticate_user(db: AsyncSession, username: str, password: str):
+    user = await get_user(db, username)
+    if not user or not verify_password(password, user.password_hash):
+        return False
+    return user
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(db, username)
+    if user is None:
+        raise credentials_exception
+    return User(username=user.username, role=user.role)
+
+def require_role(required_role: str):
+    def role_checker(current_user: User = Depends(get_current_user)):
+        if current_user.role != required_role:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Insufficient permissions"
+            )
+        return current_user
+    return role_checker
+
 
 #----------БАЗОВЫЕ CRUDS----------
 #Cruds for Tractor
