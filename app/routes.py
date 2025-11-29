cefbcf6fabdf119fdc23a876743901fbe82409c8
krymkovdv <s89232844224@gmail.com>
2025-11-29 13:13:58 +0300

    micro_fix_db

--- app/routes.py
+++ app/routes.py
@@ -108,7 +108,7 @@ def get_component(session: Session = Depends(get_session)):
             detail=f"Неизвестная ошибка: {str(e)}"
         )
 
-@router.post("/component/", response_model=schemas.ComponentSchema, status_code=status.HTTP_201_CREATED, dependencies=[Depends(require_role("moderator"))])
+@router.post("/component/", response_model=schemas.ComponentSchema, status_code=status.HTTP_201_CREATED)
 def create_component(component: schemas.ComponentSchema, db: Session = Depends(get_session)):
     # Проверка на дубликат terminal_id
     if CRUDs.get_component_by_terminal(db, component.id):
@@ -248,8 +248,8 @@ def delete_Software2Components(id: int, db: Session = Depends(get_session)):
 
 #3-я страница
 #Поиск по фильтрам КОМПОНЕНТОВ
-@router.post("/component-info")
-def get_component_by_filters(filters: schemas.ComponentInfoRequest, db: Session = Depends(get_session), response_model=List[schemas.ComponentSearchResponseItem]):
+@router.post("/component-info", response_model=List[schemas.ComponentSearchResponseItem])
+def get_component_by_filters(filters: schemas.ComponentInfoRequest, db: Session = Depends(get_session)):
     query = CRUDs.get_component_by_filters(
         db,
         trac_model=filters.trac_model,
@@ -366,3 +366,13 @@ def check_software_file(id: int, db: Session = Depends(get_session)):
         "size": file_info.size_bytes,
         "path": file_info.full_path
     }
+
+
+@router.post("/component-models")
+def get_component_models(request: schemas.RequestModel, db: Session = Depends(get_session)):
+    models_list = CRUDs.get_agg_by_trac_and_comp(
+        db, 
+        request.trac_model if request.trac_model else None, 
+        request.type_comp if request.type_comp else None
+    )
+    return {"component_models": models_list}
