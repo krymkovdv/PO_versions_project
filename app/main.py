63cdf14da88bfffd0f42a3e10c878b131052df98
krymkovdv <s89232844224@gmail.com>
2026-01-23 15:37:32 +0300

    CORS change

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="Сервис контроля версий")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],  
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"]
