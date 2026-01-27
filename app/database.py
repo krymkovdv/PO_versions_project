a2f7410a42cff0aa9b2b7a3e39e6d2f976d2b8ec
YobieYo <YobieYo2@gmail.com>
2026-01-27 12:15:59 +0300

    echo-FALSE

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 url_db = settings.get_url()
-engine = create_engine(url_db, echo= True)
+engine = create_engine(url_db)
 SessionLocal = sessionmaker(bind=engine)
 
 def get_session():
