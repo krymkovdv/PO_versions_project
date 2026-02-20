582b16230cce11728d4b78ec6db2fc38c7798b2e
KinetovZhan <zhanfikinetovfi@gmail.com>
2026-02-20 13:38:31 +0300

    origins дополнил

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="Сервис контроля версий")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:3000", "http://26.77.162.134:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3000"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"]
