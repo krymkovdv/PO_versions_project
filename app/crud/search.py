cf68051c4a2d54060d6165590c5a38a596343fb9
YobieYo <YobieYo2@gmail.com>
2026-04-07 08:37:50 +0300

    again_fix

--- app/crud/search.py
+++ app/crud/search.py
@@ -235,7 +235,7 @@ def get_archive_component_by_filters(
     return [
         {
             "name": r.name,
-            "download_link": r.download_link[33:],
+            "download_link": r.download_link[33:] if r.download_link is not None else "",
             "download_link_instruction": getattr(r, 'download_link_instruction', None),
             "type_component": r.type,
             "release_date": r.release_date.isoformat() if r.release_date else None,
